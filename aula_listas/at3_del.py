242be196a34b157c39d6c70e9e91e77b1f8d4069
Jonatan_Lima <jonatanrobertodl@gmail.com>
2025-08-14 20:15:11 -0300

    editado

--- aula_listas/at3_del.py
+++ aula_listas/at3_del.py
@@ -1,6 +1,8 @@
 #criando a listona da at2
 listona=['goiaba', 'uva', 'morango', 'brigadeiro', 'bem casado', 'beijinho', 'feijão', 'orelha de porco', 'rabo de porco', 'calabresa', 'guaraná', 'cerveja', 'vodka']
 
+print(listona)
+
 #utilizando del para remover todos os itens da listona
 del listona[:]
 
